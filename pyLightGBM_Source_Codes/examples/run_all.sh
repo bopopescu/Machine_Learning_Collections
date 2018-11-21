d5114e0e842de4684f2b272db3a6777de9d8397d
pmnyc <pmiori@gmail.com>
2018-11-20 23:13:12 -0500

    trivial

--- pyLightGBM_Source_Codes/examples/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-python binary_classification.py
-python binary_multiclass_comparison.py
-python multiclass_classification.py
-python classification_grid_search.py
-python regression.py
-python regression_grid_search.py
-python find_best_round.py
-python feature_importance.py
